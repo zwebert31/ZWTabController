c8523b62f9fc90db4056066b566cfb8ad6f9f08c
Zachary Webert <zacharywebert@Zacharys-MacBook-Air.local>
2015-01-11 12:13:11 -0500

    Initial Commit

---
+++ ZWTabController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZWTabController
+//
+//  Created by Zachary Webert on 1/11/15.
+//  Copyright (c) 2015 Zachary Webert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
